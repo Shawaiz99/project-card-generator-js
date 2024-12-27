9e25615c7b046e58c04cf8c14ab4ca721ea094e0
Shawaiz <shawaizy2k@gmail.com>
2024-12-27 14:45:14 -0500

    Forgot to push. Bug involving values and suits color not matching resolved

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+window.onload = function () {
+const cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+const cardSuits = ["♥", "♦", "♠", "♣"];
+
+    // Get random value and suit
+    const value = cardValues[Math.floor(Math.random() * cardValues.length)];
+    const suit = cardSuits[Math.floor(Math.random() * cardSuits.length)];
+
+
+    let topValueElement = document.getElementById("top-value");
+    let bottomValueElement = document.getElementById("bottom-value");
+    let suitElement = document.getElementById("center-suit");
+
+    topValueElement.textContent = value;
+    bottomValueElement.textContent = value;
+    suitElement.textContent = suit;
+
+
+    if (suit === "♥" || suit === "♦") {
+        topValueElement.style.color = "red";
+        bottomValueElement.style.color = "red";
+        suitElement.style.color = "red";
+    } else {
+        topValueElement.style.color = "black";
+        bottomValueElement.style.color = "black";
+        suitElement.style.color = "black";
+    }
+
+};
